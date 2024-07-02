141a5679aff4032afa575724aeb12c074345e1cf
Jeric June Logan <loganjeric25@gmail.com>
2024-07-02 15:19:36 +0800

    initialize vite, install tailwind and primevue

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+createApp(App)
+    .use(PrimeVue, {
+        theme: {
+            preset: Aura,
+            options: {
+                cssLayer: {
+                    name: 'primevue',
+                    order: 'tailwind-base, primevue, tailwind-utilities'
+                }
+            }
+        }
+    })
+    .mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "./src/**/**.{vue,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-primeui')
+  ],
+}
+
