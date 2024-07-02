ff7bdcb49f529064643ede61c7129858ffa52991
Jeric June Logan <loganjeric25@gmail.com>
2024-07-02 15:19:59 +0800

    import remixicon

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
 import './style.css'
+import 'remixicon/fonts/remixicon.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
 
 import PrimeVue from 'primevue/config';
